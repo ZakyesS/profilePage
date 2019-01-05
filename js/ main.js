0bc5bf5abb1a030a3a5ae64a8671cc4891fd807e
Yuneysa S <ysuarez@20h.es>
2019-01-05 23:50:23 +0000

    Fixed error when click on project's images

--- js/ main.js
+++ js/ main.js
@@ -1,4 +1,22 @@
-// MAIN
+/**
+ * Append a new HTML row into the specific container with the user data
+ * @function createProjectsUrls
+ * @public
+ * @param {object} img
+ */
+function createProjectsUrls(img) {
+  $(img).before(
+    `<div class="project-urls" style="display: grid; grid-gap: 10px; grid-template-columns: repeat(2, auto); padding: 0 0 3% 0;" project=${$(
+      img
+    ).attr("alt")}><a href="https://${$(img).attr(
+      "data-project"
+    )}" target="_blank rel="noopener noreferrer><i class="fas fa-project-diagram"></i> Page Url</a> <a href="https://github.com/${$(
+      img
+    ).attr(
+      "data-github"
+    )}" target="_blank rel="noopener noreferrer><i class="fab fa-github"></i> Github</a></div>`
+  );
+}
 
 $("table.skills tr td img").on({
   mouseenter: e => {
@@ -27,21 +45,22 @@ $("table.projects tr td img").on({
     $("span.project-description").remove();
   },
   click: e => {
-    if ($("div.project-urls").length === 0) {
-      $(e.target).before(
-        `<div class="project-urls" style="display: grid; grid-gap: 10px; grid-template-columns: repeat(2, auto); padding: 0 0 3% 0;"><a href="https://${$(
-          e.target
-        ).attr(
-          "data-project"
-        )}" target="_blank rel="noopener noreferrer><i class="fas fa-project-diagram"></i> Page Url</a> <a href="https://github.com/${$(
-          e.target
-        ).attr(
-          "data-github"
-        )}" target="_blank rel="noopener noreferrer><i class="fab fa-github"></i> Github</a></div>`
-      );
-      $("div.project-urls").on("click", () => {
-        $("div.project-urls").remove();
-      });
+    while ($("div.project-urls").length === 0) {
+      createProjectsUrls(e.target);
     }
+    if (
+      $("div.project-urls").length !== 0 &&
+      $("div.project-urls").attr("project") !== $(e.target).attr("alt")
+    ) {
+      // remove the previous div.project, the last you click before the actual click.
+      let remove = $("div.project-urls").attr("project");
+      $(`div.project-urls[project='${remove}']`).remove();
+      // create the div.project of actual click.
+      createProjectsUrls(e.target);
+    }
+
+    $("div.project-urls").on("click", () => {
+      $("div.project-urls").remove();
+    });
   }
 });
